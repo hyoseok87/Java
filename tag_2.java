409ff7aac404a37d40934a40f2a776595c125a52
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-02-21 08:35:22 +0100

    Day_2

---
+++ tag_2.java
@@ -0,0 +1,37 @@
+
+public class tag_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		? Operator
+//		int a=2;
+//		int b=5;
+//				
+//int c= a < b? ++a : ++ b;
+//System.out.println(c);
+		
+		// If-Verzweinung
+// Schleifen mit break, continue und labels
+		//While, do , for
+		
+//Arrays
+////		Objektreferenz / ab mit new in der Quellecode ist Objekt
+//		int[] array =new int[10];
+//		array[0] = 10;
+//		array[1] = 9;
+//		array[2] = 8;
+//		array[3] = 7;
+//		array[4] = 6;
+//		array[5] = 5;
+//		array[6] = 4;
+//		array[7] = 3;
+//		array[8] = 2;
+//		array[9] = 1;
+//		
+//		int[]  neuesArray = array;
+//	
+	
+		
+	}
+
+}
